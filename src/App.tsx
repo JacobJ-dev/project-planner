46f625357d5fd3a396787d72089b987cc08c7156
jj184 <jj184@students.waikato.ac.nz>
2025-10-25 23:21:31 +1300

    Created the header element for the site and added to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Component Imports
+import Header from './components/Header.tsx';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div id="header">
+        <Header/>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Project Planner</h1>
+            <h4>Sponsored by the One Piece</h4>
+        </header>
+    )
+}
+
+export default Header;
